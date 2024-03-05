7b45b3642fc0145b2433a651475997775a7d065e
arorachirag <96038346+arorachirag@users.noreply.github.com>
2024-03-05 10:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let inputs = document.querySelectorAll("input");
 
+	let firstName = inputs[0];
+	let lastName = inputs[1];
+
+	let fullName = firstName +" "+lastName;
+	alert(fullName)
 }
